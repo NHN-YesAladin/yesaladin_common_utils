d869b2a11c770dc470192f5349f44c26b168c47d
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-30 14:28:14 +0900

    ResponseDto 추가

---
+++ src/test/java/shop/yesaladin/common/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
