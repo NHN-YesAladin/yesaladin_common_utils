68389b863b8e5a60a96407b1ea9e7e1aeec09a42
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-30 14:55:14 +0900

    builder delombok

--- src/main/java/shop/yesaladin/common/dto/ResponseDto.java
+++ src/main/java/shop/yesaladin/common/dto/ResponseDto.java
@@ -3,7 +3,6 @@ package shop.yesaladin.common.dto;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,7 +13,6 @@ import org.springframework.http.HttpStatus;
  * @param <T> 응답 데이터 타입
  */
 @Getter
-@Builder
 @RequiredArgsConstructor
 public class ResponseDto<T> {
 
@@ -24,8 +22,54 @@ public class ResponseDto<T> {
     private final T data;
     private final List<String> errorMessages;
 
+    public static <T> ResponseDtoBuilder<T> builder() {
+        return new ResponseDtoBuilder<T>();
+    }
+
     @JsonGetter
     public int getStatus() {
         return this.status.value();
+    }
+
+
+    public static class ResponseDtoBuilder<T> {
+
+        private boolean success;
+        private HttpStatus status;
+        private T data;
+        private List<String> errorMessages;
+
+        ResponseDtoBuilder() {
+        }
+
+        public ResponseDtoBuilder<T> success(boolean success) {
+            this.success = success;
+            return this;
+        }
+
+        public ResponseDtoBuilder<T> status(HttpStatus status) {
+            this.status = status;
+            return this;
+        }
+
+        public ResponseDtoBuilder<T> data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public ResponseDtoBuilder<T> errorMessages(List<String> errorMessages) {
+            this.errorMessages = errorMessages;
+            return this;
+        }
+
+        public ResponseDto<T> build() {
+            return new ResponseDto<T>(success, status, data, errorMessages);
+        }
+
+        public String toString() {
+            return "ResponseDto.ResponseDtoBuilder(success=" + this.success + ", status="
+                    + this.status
+                    + ", data=" + this.data + ", errorMessages=" + this.errorMessages + ")";
+        }
     }
 }
